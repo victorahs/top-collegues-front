9003d9db3994bc18ac7649876ba959b5fdbf83a7
victorahs <victorahsene90@gmail.com>
2018-04-18 10:26:51 +0200

    add collegue component

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Collegue } from '../models';
+import { Collegue, Avis } from '../models';
 
 @Component({
   selector: 'app-collegue',
@@ -8,6 +8,18 @@ import { Collegue } from '../models';
 })
 export class CollegueComponent implements OnInit {
   @Input() collegue : Collegue;
+
+  choix = " ";
+  testAvis(avis: Avis){
+ 
+   if( avis == "detester"){
+     this.choix = "vous avez cliquer sur détester";
+     
+   }else{
+     this.choix = "Vous avez cliquer sur aimer";
+   }
+ 
+  }
 
   constructor() { }
 
