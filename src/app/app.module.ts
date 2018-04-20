29a29d2374f07addee871c7bcf4f2d34c2bd0f1e
victorahs <victorahsene90@gmail.com>
2018-04-20 17:40:21 +0200

    delete import historiqueVote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { MDBBootstrapModule } from "angular-bootstrap-md";
-
 import { AppComponent } from "./app.component";
 import { DemoComponent } from "./demo/demo.component";
 import { BandeauComponent } from "./bandeau/bandeau.component";
 import { AvisComponent } from "./avis/avis.component";
 import { CollegueComponent } from "./collegue/collegue.component";
 import { ListeColleguesComponent } from "./liste-collegues/liste-collegues.component";
-import { HistoriqueVotesComponent } from "./historique-votes/historique-votes.component";
 import { HttpClientModule } from "@angular/common/http";
 import { ColleguesService } from "./services/collegues.service";
 import { RouterModule, Routes } from "@angular/router";
 import { AccueilComponent } from "./accueil/accueil.component";
-import { DetailsCollegueComponent } from './details-collegue/details-collegue.component';
+import { DetailsCollegueComponent } from "./details-collegue/details-collegue.component";
 
 const appRoutes: Routes = [
   { path: "accueil", component: AccueilComponent },
